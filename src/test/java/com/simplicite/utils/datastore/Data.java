6d555956974a8bad224afacc1f3106b123099944
Greg <gregoire.gally@epita.fr>
2021-09-23 18:12:00 +0200

    add library fasterxml for jackson, add a data stockage with JSON

--- src/test/java/com/simplicite/utils/datastore/Data.java
+++ src/test/java/com/simplicite/utils/datastore/Data.java
@@ -3,6 +3,8 @@ package com.simplicite.utils.datastore;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 @JsonSerialize
-public record DataContainer(ArrayList<Object> objects){}
+public record Data(String name, ArrayList<Map<String, Object>> data) {
+}
--- src/test/java/com/simplicite/utils/datastore/DataStore.java
+++ src/test/java/com/simplicite/utils/datastore/DataStore.java
@@ -1,11 +1,41 @@
 package com.simplicite.utils.datastore;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.JsonArray;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class DataStore {
 
     ArrayList<Map<String, Object>> data = new ArrayList<>();
 
+    JsonArray ar = new JsonArray();
+    @Test
+    public void test() {
+        var data = new DataContainer(new ArrayList<>());
+        try {
+            // create Gson instance
+            ObjectMapper mapper = new ObjectMapper();
+            var des = mapper.readValue(Paths.get("src/test/resources/MetaData.json").toFile(), DataContainer.class);
+            System.out.println(des);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 
+    @Test
+    public void test2() throws IOException {
+        var data = new DataContainer(new ArrayList<>());
+        String content = Files.readString(Paths.get("src/test/resources/MetaData.json"));
+        JSONObject a = new JSONObject(content);
+        JSONArray array = a.getJSONArray("objects");
+    }
 }
