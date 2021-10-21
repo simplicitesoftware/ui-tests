4edf5753aaf7b23524ffd583b8adddb0cddae562
Greg <gregoire.gally@epita.fr>
2021-10-21 15:21:09 +0200

    bouton scroll v5

--- src/test/java/com/simplicite/menu/templateeditor/TemplateEditor.java
+++ src/test/java/com/simplicite/menu/templateeditor/TemplateEditor.java
@@ -32,7 +32,7 @@ public class TemplateEditor {
 
         SelenideElement bouton = area.findAll("button").shouldBe(CollectionCondition.sizeGreaterThan(0)).last();
 
-        bouton.scrollTo();
+        bouton.scrollIntoView("{behavior: \"instant\", inline: \"center\"}");
 
         SelenideElement element = area.findAll("[data-menu=\"" + type + "\"]").shouldBe(CollectionCondition.sizeGreaterThan(0)).last();
         while(!element.isDisplayed()){
